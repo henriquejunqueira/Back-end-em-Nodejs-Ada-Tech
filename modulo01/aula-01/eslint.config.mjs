b4c5ec13b38feb375b840d6634ea73ad9d81417d
Henrique Junqueira <henriquejunqueira15@gmail.com>
2024-05-14 12:47:39 -0300

    aula controle de fluxo

--- modulo01/aula-01/eslint.config.mjs
+++ modulo01/aula-01/eslint.config.mjs
@@ -1,11 +1,11 @@
-import config from "eslint-config-standard";
+import config from 'eslint-config-standard';
 
 export default [
   ...[].concat(config),
   {
     rules: {
-      semi: "error",
-      "prefer-const": "error",
+      semi: 'error',
+      'prefer-const': 'error',
       singleQuote: true,
       tabWidth: 2,
       useTabs: false,
